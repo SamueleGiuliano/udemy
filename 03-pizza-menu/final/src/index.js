8a18ddef1025d4c438ab4e34ba2fe59ddaf0e02a
Samuele Giuliano <samuele.giuliano@atlantica.it>
2023-06-22 10:50:25 +0200

    modulo 3 react con file integrati

---
+++ 03-pizza-menu/final/src/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Il Menù:</h2>
+      <Pizza />
+    </div>
+  );
+}
+
+//UTILIZZANDO JSX:
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString()}. Siamo aperti!</footer>;
+}
+// SENZA JSX:
+// function Footer() {
+//   return React.createElement("footer", null, "Siamo chiusi");
+// }
+
+function Pizza() {
+  return (
+    <div>
+      <h2>Margherita</h2>
+      <p>Pomodoro e mozzarella</p>
+      <img src="pizzas/margherita.jpg" alt="Margherita" />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
